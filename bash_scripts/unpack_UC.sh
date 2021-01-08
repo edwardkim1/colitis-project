18ce6e403c4378f6f6955e36a63204c72204a6b5
edwardkim1 <edward.kj1299@hotmail.com>
2021-01-07 23:55:03 -0600

    untar UC dataset from UCSD Chang lab

---
+++ bash_scripts/unpack_UC.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+currLoc="$PWD"
+path="${currLoc}"
+new_dir_name=$1
+
+cd ${currLoc}
+
+echo -n "extracting tar into ${new_dir_name}..."
+
+#EXTRACT TAR IN DESIRED DIRECTORY AND GUNZIP EVERYTHING
+mkdir $new_dir_name
+tar -xf GSE134809_RAW.tar -C $path/$new_dir_name
+cd $path/$new_dir_name
+echo "done"
+echo -n "gunzip..."
+gunzip *
+echo "done"
+echo "extraction completed"
